0e8b15a425bb832e2bbae7c51cc2b45449981b46
marius <wangone2008@gmail.com>
2023-11-06 14:50:45 +0800

    Modify script

--- run_rollup_relayer.sh
+++ run_rollup_relayer.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-if [ -z $GENESIS ];then 
-    geth --datadir $datadir  init $GENESIS
+if [ $IMPORT_GENESIS ];then 
+    rollup_relayer --config $1 --import-genesis
+else
+    rollup_relayer --config $1
 fi
-
-geth --networkid $networkid --mine --datadir $datadir --password $password --allow-insecure-unlock --gcmode archive   --unlock $unlockAccount   --http --http.addr "0.0.0.0" --http.port 8545 --http.vhosts "*"  --http.api "eth,scroll,net,web3,debug"   --ws --ws.addr "0.0.0.0" --ws.port 8546 --ws.api "eth,scroll,net,web3,debug" --l1.endpoint $l1_endpoint --l1.sync.startblock $startblock --l1.confirmations $confirmations
